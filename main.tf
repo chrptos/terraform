0362337f1720e12c77a0c3ceb104dca2bb4bd793
chrptos <chrptos@gmail.com>
2022-10-24 20:39:58 +0900

    VPC作成

--- main.tf
+++ main.tf
@@ -2,21 +2,21 @@
 # terraform configuration
 # ------------------------------
 terraform {
-    required_version = ">=0.13"
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~>3.0"
-        }
+  required_version = ">=0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
     }
+  }
 }
 
 # ------------------------------
 # Providers
 # ------------------------------
 provider "aws" {
-    profile = "terraform"
-    region = "ap-northeast-1"
+  profile = "terraform"
+  region  = "ap-northeast-1"
 }
 
 # ------------------------------
