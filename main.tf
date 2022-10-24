10505fd02f6b8cfca084f025a88f495f597f8ffe
chrptos <chrptos@gmail.com>
2022-10-24 20:25:27 +0900

    雛形を作成

--- main.tf
+++ main.tf
@@ -1,10 +1,35 @@
+# ------------------------------
+# terraform configuration
+# ------------------------------
+terraform {
+    required_version = ">=0.13"
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~>3.0"
+        }
+    }
+}
+
+# ------------------------------
+# Providers
+# ------------------------------
 provider "aws" {
     profile = "terraform"
     region = "ap-northeast-1"
 }
 
-resource "aws_instance" "hello_world" {
-    ami = "ami-078296f82eb463377"
-    instance_type = "t2.micro"
+# ------------------------------
+# Variables
+# ------------------------------
+variable "project" {
+  type        = string
+  default     = ""
+  description = "description"
 }
 
+variable "environment" {
+  type        = string
+  default     = ""
+  description = "description"
+}
